16590b58b446c030ed16862c7907cf261c3e7b04
Driss Laaziri <idrisslaaziri@gmail.com>
2025-03-18 09:03:27 -0400

    Create connection

---
+++ dbcon.php
@@ -0,0 +1,17 @@
+<?php 
+
+    define("HostName","localhost");
+    define("USERNAME","root");
+    define("PASSWORD","");
+    define("DATABASE","crud_operation");
+
+    
+    $connection = mysqli_connect(HostName,USERNAME,PASSWORD,DATABASE);
+
+    if(!$connection){
+        die("Connection failed !!");
+    }
+    else{
+        echo"Yess";
+    }
+?>
